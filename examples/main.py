9355d2432155a1f48e81058515a79d6abec3c6cb
sglvladi <sglvladi@gmail.com>
2022-09-18 00:32:29 +0100

    Backup

--- examples/main.py
+++ examples/main.py
@@ -1,10 +1,21 @@
+import logging
 import json
+
+import asyncio
 
 from pysblibs.config import ConfigManager
 from pysblibs.madoka import MadokaController
 from pysblibs.mqtt import MqttController
 from pysblibs.utils import register_device, get_mac_address
 
+logger = logging.getLogger('pysblibs.base')
+formatter = logging.Formatter(
+    "(%(asctime)s) [%(levelname)s:%(module)s:%(lineno)d]: %(message)s",
+    "%H:%M:%S")
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(stream_handler)
 
 def mqtt_callback(self, client, userdata, message):
     self._logger.info(f"Message received on topic {message.topic}")
@@ -18,31 +29,40 @@ def mqtt_callback(self, client, userdata, message):
             madoka.set_fan_speed(commands['fan_speed']['cool'], commands['fan_speed']['heat'])
 
 
+async def main(madoka):
+
+    config = ConfigManager('config.json')
+
+    mac = get_mac_address()
+    data = register_device(config['reg_token'], mac)
+    config.set('user_id', data['user_id'])
+    config[('mqtt', 'host')] = data['mqtthost']
+    config[('mqtt', 'port')] = data['mqttport']
+    config[('mqtt', 'user')] = data['mqttusername']
+    config[('mqtt', 'password')] = data['mqttpassword']
+
+    mqtt_client = MqttController(config['mqtt']['host'],
+                                 int(config['mqtt']['port']),
+                                 config['mqtt']['user'],
+                                 config['mqtt']['password'],
+                                 config['user_id'])
+
+    mqtt_client.on_message_cb = mqtt_callback
+    mqtt_client.connect()
+
+    print(mqtt_client._get_base_topics())
+
 madoka_mac_address = 'B8:F2:55:01:25:E0'
-
-config = ConfigManager('config.json')
-
-mac = get_mac_address()
-data = register_device(config['reg_token'], mac)
-config.set('user_id', data['user_id'])
-config[('mqtt', 'host')] = data['mqtthost']
-config[('mqtt', 'port')] = data['mqttport']
-config[('mqtt', 'user')] = data['mqttusername']
-config[('mqtt', 'password')] = data['mqttpassword']
-
-mqtt_client = MqttController(config['mqtt']['host'],
-                             int(config['mqtt']['port']),
-                             config['mqtt']['user'],
-                             config['mqtt']['password'],
-                             config['user_id'])
-
-mqtt_client.on_message_cb = mqtt_callback
-mqtt_client.connect()
-
 madoka = MadokaController(madoka_mac_address)
-
-print(mqtt_client._get_base_topics())
-
+loop = asyncio.get_event_loop()
+try:
+    asyncio.ensure_future(main(madoka))
+    loop.run_forever()
+except KeyboardInterrupt:
+    logger.info("User stopped program.")
+finally:
+    logger.info("Disconnecting...")
+    loop.run_until_complete(madoka._controller.stop())
 
 while True:
     pass
--- pysblibs/madoka.py
+++ pysblibs/madoka.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from pymadoka import force_device_disconnect, discover_devices, FanSpeedStatus, FanSpeedEnum
+from pymadoka.connection import ConnectionStatus
 from pymadoka.controller import Controller
 
 from .base import Base
@@ -14,15 +15,18 @@ class MadokaController(Base):
         self._controller = Controller(self.mac_address)
 
     def set_fan_speed(self, cool_speed: str, heat_speed: str):
-        return asyncio.run(self._set_fan_speed(cool_speed, heat_speed))
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(self._set_fan_speed(cool_speed, heat_speed))
 
     async def _connect(self):
         await force_device_disconnect(self._controller.connection.address)
         await discover_devices()
         await self._controller.start()
+        return
 
     async def _set_fan_speed(self, cool_speed: str, heat_speed: str):
-        await self._connect()
+        if self._controller.connection.connection_status != ConnectionStatus.CONNECTED:
+            await self._connect()
         fan_speed_status = FanSpeedStatus(FanSpeedEnum[cool_speed], FanSpeedEnum[heat_speed])
         return await self._controller.fan_speed.update(fan_speed_status)
 
